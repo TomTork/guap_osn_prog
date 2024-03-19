96d52171ec2f8908f39bd1b1410f7371775f303e
TomTork <52648973+TomTork@users.noreply.github.com>
2024-03-19 23:25:09 +0500

    Update ex3x1.cpp

--- ex3x1.cpp
+++ ex3x1.cpp
@@ -5,28 +5,30 @@
 
 using namespace std;
 
-bool issimple(int n){
-    for(int i = 2; i < sqrt(n) + 1; i++){
-        if(n % i == 0) return false;
+bool issimple(int n) {
+    for (int i = 2; i < sqrt(n) + 1; i++) {
+        if (n % i == 0) return false;
     }
     return true;
 }
 
-int search_index_nearest_prime_number(int*& mas, int _size, int currentIndex, int step, int deep = 0){
-    if(currentIndex < 0 or currentIndex >= _size) return -1;
-    else if(issimple(mas[currentIndex])) {cout << "DEEP " << deep << endl; return currentIndex;}
+int search_index_nearest_prime_number(int*& mas, int _size, int currentIndex, int step, int deep = 0) {
+    cout << "ОТЛАДКА: " << "глубина:" << deep << " " << "текущий индекс:" << currentIndex << endl;
+
+    if (currentIndex < 0 or currentIndex >= _size) return -1;
+    else if (issimple(mas[currentIndex])) { cout << "DEEP " << deep << endl; return currentIndex; }
     return search_index_nearest_prime_number(mas, _size, currentIndex + step, step, deep + 1);
 }
 
-int answer(int*& mas, int _size, int index){
+int answer(int*& mas, int _size, int index) {
     int m1 = search_index_nearest_prime_number(mas, _size, index, 1);
     int m2 = search_index_nearest_prime_number(mas, _size, index, -1);
-    if(m1 == -1 and m2 != -1) return m2;
-    else if(m2 == -1 and m1 != -1) return m1;
-    else if(m1 != -1 and m2 != -1){
+    if (m1 == -1 and m2 != -1) return m2;
+    else if (m2 == -1 and m1 != -1) return m1;
+    else if (m1 != -1 and m2 != -1) {
         int b1 = abs(index - m1);
         int b2 = abs(index - m2);
-        if(b1 <= b2) return m1;
+        if (b1 <= b2) return m1;
         return m2;
     }
     return -1;
@@ -34,7 +36,7 @@ int answer(int*& mas, int _size, int index){
 
 int main()
 {
-    
-    int* mas = new int[9]{ 3, 21, 35, 7, 31, 4, 4, 8, 7 };
-    cout << "RESULT:" << answer(mas, 9, 6) << endl;
+    setlocale(LC_ALL, "Russian");
+    int* mas = new int[9]{ 3, 21, 35, 7, 31, 4, 6, 8, 9 };
+    cout << "RESULT:" << answer(mas, 9, 7) << endl;
 }
