effac789d672941eb35618166e5315dab00dc6d8
TomTork <52648973+TomTork@users.noreply.github.com>
2024-04-10 18:13:32 +0300

    Update ex4x2.cpp

--- ex4x2.cpp
+++ ex4x2.cpp
@@ -5,12 +5,12 @@
 
 using namespace std;
 
-struct Object{
+struct Object{ //Структура для корзин и предметов
     float volume = 0;
     float weight = 0;
 };
 
-float searchMax(float* mas, int size){
+float searchMax(float* mas, int size){ //Функция для поиска максимума в массиве
     float ma = __FLT_MIN__;
     for(int i = 0; i < size; i++){
         if(mas[i] > ma) ma = mas[i];
@@ -18,13 +18,14 @@ float searchMax(float* mas, int size){
     return ma;
 }
 
+//Стратегия: последовательно раскладываем перебираемые значения по каждой из корзин, сначала в 1, во 2 и т.д., пока не закончатся предметы
 Object* funcPlacingItemsInBaskets(int N, Object* items, int K, float Vmax){
-    Object* allIters = new Object[N * N * K];
-    int globalIndex = 0;
-    Object* iter = new Object[K];
-    Object* timeMas = new Object[N];
-    float* sumOfWeights = new float[N * N]{0};
-    int globalIndex2 = 0;
+    Object* allIters = new Object[N * N * K]; //Массив для всех возможных итераций
+    int globalIndex = 0; //индекс для allIters
+    Object* iter = new Object[K]; //временный массив для текущей итерации расклада предметов по корзинам
+    Object* timeMas = new Object[N]; //временный массив для предметов
+    float* sumOfWeights = new float[N * N]{0}; //массив всех возможных масс
+    int globalIndex2 = 0; //индекс для sumOfWeights
     for(int i = 0; i < N; i++){
         for(int j = 0; j < N; j++){
             for(int k = 0; k < K; k++) iter[k] = Object{0, 0};
@@ -32,7 +33,7 @@ Object* funcPlacingItemsInBaskets(int N, Object* items, int K, float Vmax){
                 timeMas[r] = items[r];
             }
             for(int z = 0; z < N; z++){
-                for(int k = 0; k < K; k++){
+                for(int k = 0; k < K; k++){ //кладём перебирваемые значения в последовательно по корзинам
                     if(iter[k].volume + timeMas[z].volume <= Vmax){
                         iter[k].volume += timeMas[z].volume;
                         iter[k].weight += timeMas[z].weight;
@@ -41,7 +42,7 @@ Object* funcPlacingItemsInBaskets(int N, Object* items, int K, float Vmax){
                     }
                 }
             }
-            for(int ind = 0; ind < N; ind++){
+            for(int ind = 0; ind < N; ind++){ //Проевряем не осталось ли лишних элементов, если можно положить - кладём
                 for(int k = 0; k < k; k++){
                     if(iter[k].volume + timeMas[ind].volume <= Vmax){
                         iter[k].volume += timeMas[ind].volume;
@@ -52,12 +53,12 @@ Object* funcPlacingItemsInBaskets(int N, Object* items, int K, float Vmax){
                 }
             }
             float sum = 0;
-            for(int e = 0; e < K; e++){
+            for(int e = 0; e < K; e++){ //подсчитываем сумму, заносим её в массив
                 sum += iter[e].weight;
                 allIters[globalIndex] = iter[e];
                 globalIndex++;
             }
-            swap(items[i], items[j]);
+            swap(items[i], items[j]); //swap элементов массива для полного перебора
             sumOfWeights[globalIndex2] = sum;
             globalIndex2++;
         }
@@ -70,12 +71,12 @@ Object* funcPlacingItemsInBaskets(int N, Object* items, int K, float Vmax){
         if(sumOfWeights[i] == maxSumOfWeights){
             float raz = 0;
             for(int j = 0; j < K - 1; j++){
-                raz += abs(allIters[j + i * K].weight - allIters[j + i * K + 1].weight);
+                raz += abs(allIters[j + i * K].weight - allIters[j + i * K + 1].weight); //находим разницу между соседней массой
             }
             if(raz < resultRaz){
                 resultRaz = raz;
                 for(int q = 0; q < K; q++){
-                    result[q] = allIters[i * K + q];
+                    result[q] = allIters[i * K + q]; //находя минимальную сумму разностей соседних элементов, записываем ответ
                 }
             }
         }
